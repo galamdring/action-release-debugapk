dfe31141da45561b8290d2fc98b3afcac76d3d2e
galamdring <galamdring@gmail.com>
2020-10-12 11:37:31 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 hub checkout master
-VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*-debug.apk -a ./${APP_FOLDER}/build/outputs/apk/paid/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S) 
