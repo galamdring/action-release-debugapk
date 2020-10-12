1b1365b1af192b62a575d0507a360edaafa7b65f
galamdring <galamdring@gmail.com>
2020-10-12 12:32:56 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 hub checkout master
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*-debug.apk -a ./${APP_FOLDER}/build/outputs/apk/paid/debug/*-debug.apk -m "${RELEASE_TITLE} - ${VERSION_NAME}" $(VERSION_NAME) 
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/free/debug/*-debug.apk -a ./${APP_FOLDER}/build/outputs/apk/paid/debug/*-debug.apk -m "${RELEASE_TITLE} - ${VERSION_NAME}" ${VERSION_NAME} 
